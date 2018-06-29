0f31741c1372b9ba34a0e2cb4995df1796688d83
soliloquy <pengtanglong_local@163.com>
2018-06-29 20:46:34 +0800

    --

---
+++ Example/Pods/Target Support Files/PTLPlaceholderView/PTLPlaceholderView-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_PTLPlaceholderView : NSObject
+@end
+@implementation PodsDummy_PTLPlaceholderView
+@end
---
+++ Example/Pods/Target Support Files/Pods-PTLPlaceholderView_Example/Pods-PTLPlaceholderView_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_PTLPlaceholderView_Example : NSObject
+@end
+@implementation PodsDummy_Pods_PTLPlaceholderView_Example
+@end
